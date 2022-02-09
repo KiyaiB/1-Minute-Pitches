e77a777a32ccaf70bb33a5b3d00cb1c1cf4c0005
kiyaiB <babra.kiyai@student.moringaschool.com>
2022-02-09 22:38:28 +0300

    set database uri

--- config.py
+++ config.py
@@ -3,7 +3,8 @@ import os
 class Config:
 
   
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:password@localhost/pitches'
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:password@localhost/pitches'
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") 
     UPLOADED_PHOTOS_DEST ='app/static/photos'
     SECRET_KEY = os.environ.get('SECRET_KEY')
 #  email configurations
